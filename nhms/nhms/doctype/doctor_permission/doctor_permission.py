e7ba1660990e6dccdb7b24c5f2b6155f72f32f50
S-Amine <saidi.amine.p@gmail.com>
2024-07-03 10:43:12 +0200

    comment temporarly the remove signal

--- nhms/nhms/doctype/doctor_permission/doctor_permission.py
+++ nhms/nhms/doctype/doctor_permission/doctor_permission.py
@@ -9,7 +9,7 @@ class DoctorPermission(Document):
 	def before_save(self):
 		user = frappe.get_doc(doctype="User", username=self.doctor_nin)
 		self.created_by = frappe.session.user
-		frappe.share.add("Patient", self.created_by, user, read=1, write=1, submit=1, share=1, everyone=0, notify=1)
+		frappe.share.add("Patient", self.created_by, user, read=1, write=1, submit=1, share=1, everyone=1, notify=1)
 		bilans = frappe.db.get_list('Bilan',
 					filters={
 						'patient': self.created_by
@@ -18,7 +18,7 @@ class DoctorPermission(Document):
 					as_list=True
 				)
 		for bilan in bilans :
-			frappe.share.add("Bilan", bilan[0], user, read=1, write=1, submit=1, share=1, everyone=0, notify=1)
+			frappe.share.add("Bilan", bilan[0], user, read=1, write=1, submit=1, share=1, everyone=1, notify=1)
 	# def on_trash(self):
 	# 	user = frappe.get_doc(doctype="User", username=self.doctor_nin)
 	# 	frappe.share.remove("Patient", self.created_by, user.username)
