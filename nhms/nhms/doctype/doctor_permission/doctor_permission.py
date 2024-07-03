63143061660544ad28b8e6e40d5b6afbf3f5d8eb
S-Amine <saidi.amine.p@gmail.com>
2024-07-03 10:37:49 +0200

    comment temporarly the remove signal

--- nhms/nhms/doctype/doctor_permission/doctor_permission.py
+++ nhms/nhms/doctype/doctor_permission/doctor_permission.py
@@ -8,9 +8,8 @@ from frappe.model.document import Document
 class DoctorPermission(Document):
 	def before_save(self):
 		user = frappe.get_doc(doctype="User", username=self.doctor_nin)
-		created_by = frappe.get_doc("User", frappe.session.user)
-		self.created_by = created_by.username
-		frappe.share.add("Patient", self.created_by, user, read=1, write=1, submit=1, share=1, notify=1)
+		self.created_by = frappe.session.user
+		frappe.share.add("Patient", self.created_by, user, read=1, write=1, submit=1, share=1, everyone=0, notify=1)
 		bilans = frappe.db.get_list('Bilan',
 					filters={
 						'patient': self.created_by
@@ -19,16 +18,16 @@ class DoctorPermission(Document):
 					as_list=True
 				)
 		for bilan in bilans :
-			frappe.share.add("Bilan", bilan[0], user, read=1, write=1, submit=1, share=1, notify=1)
-	def on_trash(self):
-		user = frappe.get_doc(doctype="User", username=self.doctor_nin)
-		frappe.share.remove("Patient", self.created_by, user.username)
-		bilans = frappe.db.get_list('Bilan',
-					filters={
-						'patient': self.created_by
-					},
-					fields=['name'],
-					as_list=True
-				)
-		for bilan in bilans :
-			frappe.share.remove("Bilan", bilan[0], user.username)
+			frappe.share.add("Bilan", bilan[0], user, read=1, write=1, submit=1, share=1, everyone=0, notify=1)
+	# def on_trash(self):
+	# 	user = frappe.get_doc(doctype="User", username=self.doctor_nin)
+	# 	frappe.share.remove("Patient", self.created_by, user.username)
+	# 	bilans = frappe.db.get_list('Bilan',
+	# 				filters={
+	# 					'patient': self.created_by
+	# 				},
+	# 				fields=['name'],
+	# 				as_list=True
+	# 			)
+	# 	for bilan in bilans :
+	# 		frappe.share.remove("Bilan", bilan[0], user.username)
